8ed43e93b4cf84d0e29de30d6f6d02ab1108c15f
sunJ0120 <sspure1214@gmail.com>
2025-10-24 16:04:18 +0900

    feat: InputParser를 생명 주기가 같은 InputView에 의존성 연결

--- src/main/java/racingcar/view/InputView.java
+++ src/main/java/racingcar/view/InputView.java
@@ -1,6 +1,8 @@
 package racingcar.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import java.util.List;
+import racingcar.util.InputParser;
 
 /**
  * 클래스 이름: InputView
@@ -12,14 +14,25 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     private static final String INPUT_CAR_NAMES_MESSAGE = "경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)";
     private static final String INPUT_ATTEMPT_COUNT_MESSAGE = "시도할 횟수는 몇 회인가요?";
+    private final InputParser inputParser;
 
-    public String readCarName() {
-        System.out.println(INPUT_CAR_NAMES_MESSAGE);
-        return Console.readLine();
+    public InputView(InputParser inputParser) {
+        this.inputParser = inputParser;
     }
 
-    public String readAttemptCount() {
+    public List<String> readAndParseCarNames() {
+        System.out.println(INPUT_CAR_NAMES_MESSAGE);
+        String attemptString = Console.readLine();
+        return parseCarName(attemptString);
+    }
+
+    public int readAttemptCount() {
         System.out.println(INPUT_ATTEMPT_COUNT_MESSAGE);
-        return Console.readLine();
+        String attemptCount = Console.readLine();
+        return Integer.parseInt(attemptCount);
+    }
+
+    private List<String> parseCarName(String attemptString) {
+        return inputParser.parseCarName(attemptString);
     }
 }
