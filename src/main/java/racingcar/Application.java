abde00ba9f5368d56c12b6ae3bf2f0de0280cb56
sunJ0120 <sspure1214@gmail.com>
2025-10-24 16:59:00 +0900

    feat: 자동차 경주 v1 개발 완료
    1. 대략적인 객체 내부 로직 구현
    2. 대략적인 객체 협력 관계 정의
    3. 예외 처리 및 이에 따른 기본 test는  v1.1에서 진행할 예정

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,21 @@
 package racingcar;
 
+import racingcar.controller.RacingController;
+import racingcar.domain.RandomGenerator;
+import racingcar.util.InputParser;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        InputParser inputParser = new InputParser();
+        InputView inputView = new InputView(inputParser);
+        RandomGenerator randomGenerator = new RandomGenerator();
+        RacingController racingController = new RacingController(randomGenerator);
+        OutputView outputView = new OutputView();
+
+        RacingGameManager racingGameManager = new RacingGameManager(inputView, racingController, outputView);
+        racingGameManager.run();
     }
 }
