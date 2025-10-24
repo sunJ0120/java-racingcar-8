ae721831c61b028c3ea7eecd09894d15f59248a3
sunJ0120 <sspure1214@gmail.com>
2025-10-24 16:05:06 +0900

    feat: 차 이름 파싱을 위한 InputParser 객체 기본 기능 정의

--- src/main/java/racingcar/util/InputParser.java
+++ src/main/java/racingcar/util/InputParser.java
@@ -1,10 +1,22 @@
 package racingcar.util;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * 클래스 이름: InputParser
- * 
+ * <p>
  * 버전 정보: 1.0
- * 
+ * <p>
  * 날짜: 2025-10-24
- */public class InputParser {
+ */
+public class InputParser {
+    private static final String CAR_NAME_DELIMITER = ",";
+
+    public List<String> parseCarName(String inputText) {
+        return Arrays.stream(inputText.split(CAR_NAME_DELIMITER))
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
 }
