ed8516e0682e5f5fab5f56116732bcbdd0e95c8e
sunJ0120 <sspure1214@gmail.com>
2025-10-25 19:06:01 +0900

    feat: 파싱 예외 추가 완료

--- src/main/java/racingcar/util/InputParser.java
+++ src/main/java/racingcar/util/InputParser.java
@@ -1,7 +1,9 @@
 package racingcar.util;
 
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 /**
@@ -15,8 +17,50 @@ public class InputParser {
     private static final String CAR_NAME_DELIMITER = ",";
 
     public List<String> parseCarName(String inputText) {
-        return Arrays.stream(inputText.split(CAR_NAME_DELIMITER))
+        List<String> parseCarNames = Arrays.stream(inputText.split(CAR_NAME_DELIMITER))
                 .map(String::trim)
                 .collect(Collectors.toList());
+        validate(parseCarNames);
+        return parseCarNames;
+    }
+
+    private void validate(List<String> carNames) {
+        validateListNotEmpty(carNames);
+        validateEachCarName(carNames);
+        validateDuplicateNames(carNames);
+    }
+
+    private void validateListNotEmpty(List<String> carNames) {
+        if (carNames.isEmpty()) {
+            throw new IllegalArgumentException("차 이름을 입력해주세요");
+        }
+    }
+
+    private void validateDuplicateNames(List<String> carNames) {
+        Set<String> checkNames = new HashSet<>();
+        for (String carName : carNames) {
+            if (!checkNames.add(carName)) {
+                throw new IllegalArgumentException("이름이 중복됩니다. : " + carName);
+            }
+        }
+    }
+
+    private void validateEachCarName(List<String> carNames) {
+        for (String carName : carNames) {
+            validateCarNameIsEmpty(carName);
+            validateCarNameLength(carName);
+        }
+    }
+
+    private void validateCarNameIsEmpty(String carName) {
+        if (carName.isBlank()) {
+            throw new IllegalArgumentException("차 이름은 공백일 수 없습니다.");
+        }
+    }
+
+    private void validateCarNameLength(String carName) {
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException("차 이름은 5자보다 길 수 없습니다.");
+        }
     }
 }
