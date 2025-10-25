0db43ef8e5b377aca08fe18565f6597ce707d8c6
sunJ0120 <sspure1214@gmail.com>
2025-10-25 19:07:46 +0900

    fix: RandomGenerator 의존 삭제

--- src/main/java/racingcar/controller/RacingController.java
+++ src/main/java/racingcar/controller/RacingController.java
@@ -1,7 +1,6 @@
 package racingcar.controller;
 
 import racingcar.domain.Cars;
-import racingcar.domain.RandomGenerator;
 
 /**
  * 클래스 이름: RacingController
@@ -11,14 +10,7 @@ import racingcar.domain.RandomGenerator;
  * 날짜: 2025-10-22
  */
 public class RacingController {
-    private final RandomGenerator randomGenerator;
-
-    public RacingController(RandomGenerator randomGenerator) {
-        this.randomGenerator = randomGenerator;
-
-    }
-
     public void race(Cars cars) {
-        cars.moveAllCars(randomGenerator);
+        cars.moveAllCars();
     }
 }
