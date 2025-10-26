26b001d8171fbf7dfc2d66e950310e9aa8e9bc78
sunJ0120 <sspure1214@gmail.com>
2025-10-26 16:53:36 +0900

    refactor: OutputView 리팩터링 완료
    1. 매직 스트링 상수화
    2. 메서드가 네이밍에 맞는 작은 책임만 지도록 분리

--- src/main/java/racingcar/view/OutputView.java
+++ src/main/java/racingcar/view/OutputView.java
@@ -8,27 +8,42 @@ import racingcar.domain.Cars;
 /**
  * 클래스 이름: OutputView
  * <p>
- * 버전 정보: 1.0
+ * 버전 정보: 1.1
  * <p>
- * 날짜: 2025-10-22
+ * 날짜: 2025-10-26
  */
 public class OutputView {
+    private static final String POSITION_ICON = "-";
+    private static final String NAME_POSITION_DELIMITER = " : ";
+    private static final String WINNER_DELIMITER = ", ";
+    private static final String RESULT_HEADER = "실행 결과";
+    private static final String WINNER_PREFIX = "최종 우승자 : %s";
+
     public void printRacingStart() {
         System.out.println();
-        System.out.println("실행 결과");
+        System.out.println(RESULT_HEADER);
     }
 
     public void printRoundResult(Cars cars) {
         for (Car car : cars) {
-            System.out.println(car.getName() + " : " + "-".repeat(car.getPosition()));
+            printCarStatus(car);
         }
         System.out.println();
     }
 
+    private void printCarStatus(Car car) {
+        System.out.println(car.getName() + NAME_POSITION_DELIMITER +
+                POSITION_ICON.repeat(car.getPosition()));
+    }
+
     public void printWinners(List<Car> winners) {
-        String names = winners.stream()
+        String names = formatWinnerNames(winners);
+        System.out.println(String.format(WINNER_PREFIX, names));
+    }
+
+    private String formatWinnerNames(List<Car> winners) {
+        return winners.stream()
                 .map(Car::getName)
-                .collect(Collectors.joining(", "));
-        System.out.println("최종 우승자 : " + names);
+                .collect(Collectors.joining(WINNER_DELIMITER));
     }
 }
